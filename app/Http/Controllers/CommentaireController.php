65ce207e030d3f9d5f247180426790ac265c5cdc
maori-bouchet <70749427+maori-bouchet@users.noreply.github.com>
2021-11-14 15:28:16 +0100

    CommentaireController.php

---
+++ app/Http/Controllers/CommentaireController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+namespace App\Http\Controllers;
+use App\Models\Article;
+use App\Models\Categorie;
+use App\Models\Commentaires;
+use App\Models\Log;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+class CommentaireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        $article = Article::find($id);
+        $commentaires = Commentaires::where('article_id',$id)->get();
+        return view('commentaires',compact("article","commentaires"));
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
